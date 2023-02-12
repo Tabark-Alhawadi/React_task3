7091420c22b8cc0e5e78d8e588a4881fc122df49
TabarkAlhawadi <tabarkalhawadi1@gmail.com>
2023-02-12 16:24:35 +0300

    task3

---
+++ task3/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Task3 from './components/Task3'
+
+function App() {
+  return (
+   <div>
+    <Task3 />
+   </div>
+  );
+}
+
+export default App;
